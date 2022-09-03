9a8a7c474b4b52f96ebb9758dea6db5ae3a128d5
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-09-03 14:03:57 +0900

    Fix: Grid

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 import { useEffect, useRef, useState } from "react";
 
 import { CanvasEditor } from "@/components/editor/canvas";
@@ -29,31 +29,34 @@ const Editor: NextPageWithLayout = () => {
 
   return (
     <EditorProvider>
-      <Grid templateColumns="100%" templateRows="auto 1fr" height="100vh">
+      <Grid
+        templateColumns="100%"
+        templateRows="auto 1fr"
+        height="100vh"
+        maxH="100vh"
+        width="100vw"
+        maxW="100vw"
+      >
         <HeaderEditor />
-        <Grid
-          templateColumns="85% 15%"
-          templateRows="2.5fr 1fr"
-          gap={4}
-          padding={4}
-          height="calc(100% - 1rem)"
-          width="calc(100% - 1rem)"
-        >
-          <GridItem
-            gridColumn="1 / 2"
-            gridRow="1 / 2"
-            ref={editorParentRef}
+        <Box p={4}>
+          <Grid
+            templateColumns="calc(100% - 316px) 300px"
+            templateRows="calc(100% - 316px) 300px"
+            gap={4}
+            height="100%"
             width="100%"
           >
-            <CanvasEditor maxSize={editorParentSize} />
-          </GridItem>
-          <GridItem gridColumn="1 / 2" gridRow="2 / 3" width="100%">
-            <TimelineEditor />
-          </GridItem>
-          <GridItem gridColumn="2 / 3" gridRow="1 / 3" width="100%">
-            <ParamsEditor />
-          </GridItem>
-        </Grid>
+            <GridItem gridColumn="1 / 2" gridRow="1 / 2" ref={editorParentRef}>
+              <CanvasEditor maxSize={editorParentSize} />
+            </GridItem>
+            <GridItem gridColumn="1 / 2" gridRow="2 / 3">
+              <TimelineEditor />
+            </GridItem>
+            <GridItem gridColumn="2 / 3" gridRow="1 / 3">
+              <ParamsEditor />
+            </GridItem>
+          </Grid>
+        </Box>
       </Grid>
     </EditorProvider>
   );
