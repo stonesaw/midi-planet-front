da6ec0395bb10cdef4e1e98233e863626b2c668b
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-09-15 23:42:39 +0900

    feat: next-auth + prisma

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,24 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) session.user.id = user.id;
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_KEY as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
