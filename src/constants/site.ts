50c75f2a0c9d7398a57ad36d474b81209e61e138
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-09-18 05:00:12 +0900

    fix

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,5 +1,6 @@
-export const BASE_URL =
-  `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` || "http://localhost:3000";
+export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : "http://localhost:3000";
 
 export const SITE_TITLE = "MIDI Planet";
 
